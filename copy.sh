35a94451071489bb4c1bc81b177b37c1becaeb06
Ewerton <ewertonduarte37@gmail.com>
2020-06-25 00:32:52 -0300

    implantar no google colab

--- copy.sh
+++
@@ -1,9 +0,0 @@
-name=$1
-server='AzureUser@ewerton.southcentralus.cloudapp.azure.com'
-dir='/home/AzureUser/experimentos/'
-#scp *.py AzureUser@13.84.184.250:/home/AzureUser/experimentos
-scp $server:$dir*.csv ./
-scp -r $server:$dir$name/ ./
-scp $server:$dir$name.hdf5 ./
-
-
--- send.sh
+++
@@ -1,4 +0,0 @@
-server='AzureUser@ewerton.southcentralus.cloudapp.azure.com'
-dir='/home/AzureUser/experimentos/'
-
-scp *.py $server:$dir
