6d7d128a0651b93832800de6696649366f326795
Ewerton <ewertonduarte37@gmail.com>
2020-06-25 13:02:10 -0300

    implantar no google colab

--- parameters.py
+++ parameters.py
@@ -3,7 +3,7 @@ params_exp = {
     'optimizer': 'sgd',
     'opt_params': {'learning_rate':0.01, 'momentum':0.9, 'decay':0.5e-3},
     'loss': 'categorical_crossentropy',
-    'metrics': ['categorical_accuracy', 'Precision', 'TruePositive', 'FalsePositive', 'TrueNegative', 'FalseNegative'],
+    'metrics': ['categorical_accuracy', 'Precision', 'TruePositives', 'FalsePositives', 'TrueNegatives', 'FalseNegatives'],
     'initializers': True,
     'epochs': 1 ,
     'batch_size': 256,
