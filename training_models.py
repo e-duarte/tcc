ca633a39fa2946cc8ec7a2047ef02e23d527e046
Ewerton Duarte <ewertonduarte37@gmail.com>
2020-07-03 22:43:56 -0300

    implantar no google colab

--- training_models.py
+++ training_models.py
@@ -34,6 +34,7 @@ class Trainner():
             history = model.fit(x,
                         y,
                         epochs=self.epochs,
+                        validation_split=0.1
                         # callbacks=self.callbacks,
                         # use_multiprocessing=True,
                         batch_size=self.batch_size,
