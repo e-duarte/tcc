793f97286971ff544773b11807b120da9aa2bcba
Ewerton Duarte <ewertonduarte37@gmail.com>
2020-07-06 17:22:32 -0300

    implantar no google colab

--- teste_crossvalidation.py
+++ teste_crossvalidation.py
@@ -3,10 +3,10 @@ from models import Alexnet
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 from utils import expand_dims, preprocessing, to_categorical
-# from sklearn.metrics import recall_score
+from sklearn.metrics import recall_score, precision_score, accuracy_score
 import numpy as np
 
-scoring = ['precision_macro', 'recall_macro']
+scoring = [accuracy_score, precision_score, recall_score]
 
 (train_x, train_y), (test_x, test_y) = mnist.load_data()
 
@@ -33,10 +33,10 @@ def build_model():
     return model
 
 
-cv =  ShuffleSplit(n_splits=10, random_state=0)
+cv =  ShuffleSplit(n_splits=5, random_state=0)
 model = KerasClassifier(build_model, epochs=2, batch_size=256)
 
 # scores = cross_val_score(model,  inputs, targets, cv=cv)
 scores = cross_val_validate(model,  inputs, targets, cv=cv, scoring=scoring)
 
-print(scores.mean())
+print(scores['test_accuracy_score'].mean())
